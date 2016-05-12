0ffd3560f8642111de9a088bef913c3d180641f7
John La <johnphi4747@gmail.com>
2016-05-11 22:52:15 -0400

    Made him move

--- Hero.py
+++ Hero.py
@@ -53,6 +53,14 @@ class Hero(Person):
 		# This could also be an image loaded from the disk.
 		self.image = pygame.image.load('hero_front.png').convert_alpha()
 
+		self.imagesL = [pygame.image.load('hero_left.png'), pygame.image.load('hero_left_L.png'), pygame.image.load('hero_left_R.png')]
+		self.imagesR = [pygame.image.load('hero_right.png'), pygame.image.load('hero_right_L.png'), pygame.image.load('hero_right_R.png')]
+		self.imagesU = [pygame.image.load('hero_up.png'), pygame.image.load('hero_up_L.png'), pygame.image.load('hero_up_R.png')]
+		self.imagesD = [pygame.image.load('hero_down.png'), pygame.image.load('hero_down_L.png'), pygame.image.load('hero_down_R.png')]
+		self.imageLeft = 0
+		self.imageRight = 0
+		self.imageDown = 0
+		self.imageUp = 0
 		# Fetch the rectangle object that has the dimensions of the image
 		# Update the position of this object by setting the values of rect.x and rect.y
 		self.rect = self.image.get_rect()
@@ -68,7 +76,46 @@ class Hero(Person):
 	weapon = Weapon()
 
 	seenScene = {}
-	
+	def resetImage(self, direction):
+		if direction == "left":
+			self.imageRight = 0
+			self.imageDown = 0
+			self.imageUp = 0
+		elif direction == "right":
+			self.imageLeft = 0
+			self.imageDown = 0
+			self.imageUp = 0
+		elif direction == "down":
+			self.imageRight = 0
+			self.imageLeft = 0
+			self.imageUp = 0
+		elif direction == "up":
+			self.imageRight = 0
+			self.imageDown = 0
+			self.imageLeft = 0
+
+	def moveImage(self, direction):
+		self.resetImage(direction)
+		if direction == "left":
+			self.imageLeft += 1
+			if self.imageLeft > 2:
+				self.imageLeft = 0
+			self.image = self.imagesL[self.imageLeft]
+		elif direction == "right":
+			self.imageRight += 1
+			if self.imageRight > 2:
+				self.imageRight = 0
+			self.image = self.imagesR[self.imageRight]
+		elif direction == "down":
+			self.imageDown += 1
+			if self.imageDown > 2:
+				self.imageDown = 0
+			self.image = self.imagesD[self.imageDown]
+		elif direction == "up":
+			self.imageUp += 1
+			if self.imageUp > 2:
+				self.imageUp = 0
+			self.image = self.imagesU[self.imageUp]
 	def checkScene(self, scene):
 		seenScene[scene] = True
 	def setSurface(self, surface):
